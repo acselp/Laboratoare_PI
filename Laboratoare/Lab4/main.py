14596e513c6c78a216ec61cf4218461845e31e0f
Virgiliu Plesca <virgiliu.plesca@iis.utm.md>
2021-11-14 11:46:24 +0200

    Lab 4 (1 & 2 logic done)

--- Laboratoare/Lab4/main.py
+++ Laboratoare/Lab4/main.py
@@ -1,144 +1,123 @@
-# Iata un exemplu de
-# rezultat a acestui program:
+
+
+
+
+
+# 1. Mystic 8 Ball -----------------------------------------------------------
 #
-# Intrebarea a fost... Voi deveni eu bogat?
-# Se scutura bila.
-# Apare raspunsul...
-# Bila misterioasa ne spune... Probabil da.
+# import random as rand
 #
-# Intrebarea a fost... Va cistiga Zimbru campionatul europei?
-# Se scutura bila.
-# Apare raspunsul...
-# Bila misterioasa ne spune... Probabil nu.
-
-
-# Pozitiv:
-# ● Este sigur (fără îndoială)
-# ● Este incontestabil (o concluzie dinainte)
-# ● Fără îndoială (fără îndoială)
-# ● Da - cu siguranţă (cu siguranta da)
-# ● Puteţi să vă bazaţi pe ea (Puteţi fi sigur de asta)
+# def num_prezicere(num):
+#     if num == 1:
+#         return "Este sigur"
+#     elif num == 2:
+#         return "Este incontestabil"
+#     elif num == 3:
+#         return "Fără îndoială"
+#     elif num == 4:
+#         return "Da - cu siguranţă"
+#     elif num == 5:
+#         return "Puteţi să vă bazaţi pe ea"
+#     elif num == 6:
+#         return "După cum văd eu lucrurile, da"
+#     elif num == 7:
+#         return "Cel mai probabil"
+#     elif num == 8:
+#         return "Outlook bun"
+#     elif num == 9:
+#         return "Punct de semne pentru a da"
+#     elif num == 10:
+#         return "Da"
+#     elif num == 11:
+#         return "Răspuns neclar, încercaţi din nou"
+#     elif num == 12:
+#         return "Adresaţi-vă din nou mai târziu"
+#     elif num == 13:
+#         return "Mai bine nu-ţi spun acum"
+#     elif num == 14:
+#         return "Nu se poate prezice acum"
+#     elif num == 15:
+#         return "Se concentrează şi întreabă din nou"
+#     elif num == 16:
+#         return "Nu conta pe el"
+#     elif num == 17:
+#         return  "Răspunsul meu este nu"
+#     elif num == 18:
+#         return "Sursele mele spun că nu"
+#     elif num == 19:
+#         return "Perspectivele nu sunt atît de bune"
+#     elif num == 20:
+#         return "Foarte dubios"
 #
-# Ezitant pozitiv
-# ● După cum văd eu lucrurile, da (cred - "da")
-# ● Cel mai probabil (cel mai probabil)
-# ● Outlook bun (perspective bune)
-# ● Punct de semne pentru a da (semnul spune - "da")
-# ● Da (Yes)
+# def mystic_ball():
+#     s = ""
+#     while True:
+#         s = input("Introduceti intrebarea: ")
+#         print("Intrebarea a fost... " + s)
+#         print("Se scutura bila...\nApare raspunsul...")
+#         print("Bila misterioasa ne spune... " + num_prezicere(rand.randint(1, 20)) + "\n\n")
+#         print("1. Incearca din nou\n0. Iesire")
 #
-# Neutru
-# ● Răspuns neclar, încercaţi din nou (nu este clar, încercaţi din nou)
-# ● Adresaţi-vă din nou mai târziu (întrebaţi mai târziu)
-# ● Mai bine nu-ţi spun acum (Mai bine nu spun)
-# ● Nu se poate prezice acum (Cine nu poate prezice)
-# ● Se concentrează şi întreabă din nou (concentrat şi de a pune din nou)
-#
-# Negativ
-# ● Nu conta pe el (nu cred)
-# ● Răspunsul meu este nu (Răspunsul meu - "nu")
-# ● Sursele mele spun că nu (Potrivit informaţiilor mele - "nu")
-# ● Perspectivele nu sunt atît de bune (Perspectivele nu sunt foarte bune)
-# ● Foarte dubios (îndoielnic)
-
-
-# Importa modulul random
-
-# Scrieti o functie numar_prezice
-# Aceasta va fi o funcţie ajutatoare.
-# E ava primi parametru numar si va returna un sir
-#
-# Numerele posibile pot fi de la 1 pina la 10 inclusiv.
-# fiecare numar ar trebui sa faca previziuni
-# care va fi raspunsul da sau nu.
-#
-# Sugestii de raspuns luati de mai sus.
-# 0 – Da, probabil!
-# 1 – Definitiv nu.
-# ...
-#
-# Daca se tasteaza alta cifra decit cele pina la 10
-# trebuie sa afiseze ca ceva nu este corect
-# mesaj de atentionare si reintroduce numarul.
-
-# def numar_prezice(numar):
-#
-#
-# # Scrie aici codul.
-# # Utilizeaza if...elif...else
-# # pentru a verifica numarul
-# # si a returna sirul prezis.
+#         q = int(input("\n    >>> "))
+#         if q == 1:
+#             continue
+#         elif q == 2:
+#             return
+#         else:
+#             print("Dati o valoare corecta!!!")
+#             continue
 #
 #
-# def mystical_ball(intrebarea):
+# mystic_ball()
+#
+#
 
 
-# afiseaza intrebarea initiala in consola
+# 2. -------------------------------------------
 
-# Afiseaza "Ai agitat bila misterioasa."
+def guess_game():
 
-# Utilizeaza randrange pentru a afla numarul aleator
+    # Primul jucator
+    while True:
+        numar_secret = int(input("Dati un numar secret: "))
+        if numar_secret >= 0 <= 1000:
+            break
+        else:
+            print("Eroare: Dati un numar din intervalul [0, 1000]")
 
-# Use functia ce transleaza
-# numarul selectat aleator
-# in prezicere
+    if numar_secret > 100:
+        interval = [0, 1000]
+        nincercari = 10
+    else:
+        interval = [0, 100]
+        nincercari = 7
 
+    print("Jocul a inceput. Incercati sa ghiciti numarul")
+    print("Numarul se afla in intervalul", interval, "respectiv\naveti", nincercari, "incercari pentru a ghici numarul.")
 
-# Construieste suspens prin adaugind un rind nou
-# Apare raspunsul
+    auxn = nincercari
 
+    # Ciclul principal
+    while True:
 
-# Afiseaza un rind
-# Bila ne spune...
-# si introdu prezicerea
-
-
-# afiseaza un rind nou
-
-
-
-# Testeaza programul cu intrebarile
-
-def num_prezicere(num):
-    if num == 1:
-        return "Este sigur (fără îndoială)"
-    elif num == 2:
-        return "Este incontestabil (o concluzie dinainte)"
-    elif num == 3:
-        return "Fără îndoială (fără îndoială)"
-    elif num == 4:
-        return "Da - cu siguranţă (cu siguranta da)"
-    elif num == 5:
-        return "Puteţi să vă bazaţi pe ea (Puteţi fi sigur de asta)"
-    elif num == 6:
-        return "După cum văd eu lucrurile, da (cred - 'da')"
-    elif num == 7:
-        return "Cel mai probabil (cel mai probabil)"
-    elif num == 8:
-        return "Outlook bun (perspective bune)"
-    elif num == 9:
-        return "Punct de semne pentru a da (semnul spune - 'da')"
-    elif num == 10:
-        return "Da (Yes)"
-    elif num == 11:
-        return "Răspuns neclar, încercaţi din nou (nu este clar, încercaţi din nou)"
-    elif num == 12:
-        return "Adresaţi-vă din nou mai târziu (întrebaţi mai târziu)"
-    elif num == 13:
-        return "Mai bine nu-ţi spun acum (Mai bine nu spun)"
-
-    # ●
-    # ●
-    # ●
-    # ● Nu se poate prezice acum (Cine nu poate prezice)
-    # ● Se concentrează şi întreabă din nou (concentrat şi de a pune din nou)
-    #
-    # Negativ
-    # ● Nu conta pe el (nu cred)
-    # ● Răspunsul meu este nu (Răspunsul meu - "nu")
-    # ● Sursele mele spun că nu (Potrivit informaţiilor mele - "nu")
-    # ● Perspectivele nu sunt atît de bune (Perspectivele nu sunt foarte bune)
-    # ● Foarte dubios (îndoielnic)
+        guess = int(input("\nNumarul ghicit: "))
+        nincercari -= 1
+        if nincercari == 0:
+            print("Din pacate numarul de incercari a fost epuizat. \nAti perdut :(")
+            break
+        elif guess == numar_secret:
+            print("Numarul a fost ghicit.\n Felicitari!!!")
+            print("Ati ghicit numarul din", auxn - nincercari, "incercari.")
+            break
+        elif guess > numar_secret:
+            print("Trebuie mai mic!!!")
+        elif guess < numar_secret:
+            print("Trebuie mai mare!!!")
+        print("Au ramas", nincercari, "incercari. \nIncercati din nou.")
 
 
-mystical_ball("Voi fi eu bogat?")
-mystical_ball("Va cîstiga Zimbru campionatul europei?")
+
+guess_game()
+
+
+
